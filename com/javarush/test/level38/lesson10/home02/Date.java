cbf063d270febe1ed18e6a026dd701a93cba78b5
posthuman87 <posthuman87@gmail.com>
2017-01-03 14:42:40 +0300

    work

---
+++ com/javarush/test/level38/lesson10/home02/Date.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level38.lesson10.home02;
+
+public @interface Date {
+    int year();
+
+    int month();
+
+    int day();
+
+    int hour();
+
+    int minute();
+
+    int second();
+    //напиши свой код
+}
